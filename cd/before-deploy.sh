ce8359c11d1d14d6668f2553d97d618b1bd98def
piotr-kalanski <piotr.kalanski@gmail.com>
2018-02-03 20:54:26 +0100

    Release 0.3 - travis settings

---
+++ cd/before-deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_3f486597e5e2_key -iv $encrypted_3f486597e5e2_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
+fi
